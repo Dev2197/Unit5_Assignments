0a0f34251ceaadda6af8eb712829515a648f2149
Devendra <devendrakumart8@gmail.com>
2022-04-10 11:48:46 +0530

    coomit

---
+++ react-navbar/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  let links = ["Services", "Projects", "About"]
+  return (
+    <div className="App">
+      <Logo />
+
+      <div className='links'>
+      {links.map((e)=>{
+        return <Links link={e} />
+      })}
+      </div>
+
+    <Butt />
+    </div>
+  );
+
+  
+}
+
+function Logo()
+{
+  return <img src={logo} />
+}
+
+
+function Links(link)
+{
+  return <p className='links'>{link.link}</p>
+}
+
+function Butt()
+{
+  return <button>Contact</button>
+}
+
+export default App;
